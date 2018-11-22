35200778ac66800af2f18056c2032c0a471f9db8
M150 <stevovic@gmail.com>
2018-11-22 05:41:46 +0100

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import config from '../../meta/config';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>{config.copyright}</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
