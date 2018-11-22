cf278f216cffeb03a4caf8c7fe8346cf27eec200
M150 <stevovic@gmail.com>
2018-11-22 05:43:33 +0100

    footer and meta added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import Footer from './Footer';
+import './all.sass';
+import config from '../../meta/config';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet>
+      <title>{config.siteTitle}</title>
+      <meta name="description" content={config.siteDescription} />
+    </Helmet>
     <Navbar />
     <div>{children}</div>
+    <Footer />
   </div>
-)
+);
 
-export default TemplateWrapper
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+};
+
+export default TemplateWrapper;
