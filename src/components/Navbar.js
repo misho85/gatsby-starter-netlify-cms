584f015ecf5641a3be1113867256add0482881dd
M150 <stevovic@gmail.com>
2018-11-22 05:44:53 +0100

    added dropdown func

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,78 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+class Navbar extends Component {
+  state = {
+    isActive: false
+  };
 
-export default Navbar
+  toggleDropdown = () => {
+    const toggledIsActive = this.state.isActive ? false : true;
+
+    this.setState({
+      isActive: toggledIsActive
+    });
+  };
+
+  render() {
+    return (
+      <nav
+        className="navbar is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item">
+            <figure className="image">
+              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            </figure>
+          </Link>
+          <button
+            onClick={this.toggleDropdown}
+            className={`button is-white navbar-burger ${
+              this.state.isActive ? 'is-active' : ''
+            }`}
+            data-target="navMenu"
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
+        <div
+          className={`navbar-menu ${this.state.isActive ? 'is-active' : ''}`}
+          id="navMenu"
+        >
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+            <Link className="navbar-item" to="/products">
+              Products
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+            <Link className="navbar-item" to="/contact/examples">
+              Form Examples
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <Link className="button is-primary is-outlined" to="/contact">
+                    Contact Us
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
