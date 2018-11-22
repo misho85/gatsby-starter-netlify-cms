8f36bea1236751a8be656788469cd254737a8008
M150 <stevovic@gmail.com>
2018-11-22 11:24:51 +0100

    delete examples

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import logo from '../img/logo.svg'
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import logo from '../img/logo.svg';
 
 class Navbar extends Component {
   state = {
@@ -22,50 +22,49 @@ class Navbar extends Component {
         role="navigation"
         aria-label="main navigation"
       >
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <figure className="image">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </figure>
-          </Link>
-          <button
-            onClick={this.toggleDropdown}
-            className={`button is-white navbar-burger ${
-              this.state.isActive ? 'is-active' : ''
-            }`}
-            data-target="navMenu"
-          >
-            <span />
-            <span />
-            <span />
-          </button>
-        </div>
-        <div
-          className={`navbar-menu ${this.state.isActive ? 'is-active' : ''}`}
-          id="navMenu"
-        >
-          <div className="navbar-start">
-            <Link className="navbar-item" to="/about">
-              About
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image">
+                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              </figure>
             </Link>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
+            <button
+              onClick={this.toggleDropdown}
+              className={`button is-white navbar-burger ${
+                this.state.isActive ? 'is-active' : ''
+              }`}
+              data-target="navMenu"
+            >
+              <span />
+              <span />
+              <span />
+            </button>
           </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <Link className="button is-primary is-outlined" to="/contact">
-                    Contact Us
-                  </Link>
-                </p>
+          <div
+            className={`navbar-menu ${this.state.isActive ? 'is-active' : ''}`}
+            id="navMenu"
+          >
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Products
+              </Link>
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="field is-grouped">
+                  <p className="control">
+                    <Link
+                      className="button is-primary is-outlined"
+                      to="/contact"
+                    >
+                      Contact
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
